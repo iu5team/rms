f6573a7bc476a79b4f3a9b8909e119ff1e15d3c2
alekseyl1992 <alekseyl@list.ru>
2016-04-18 00:09:17 +0300

    Code generation

---
+++ gen/server/views/EmployeesTasksView.java
@@ -0,0 +1,23 @@
+package views;
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class EmployeesTasksView {
+
+    /**
+     * Default constructor
+     */
+    public EmployeesTasksView() {
+    }
+
+    /**
+     * 
+     */
+    public void get() {
+        // TODO implement here
+    }
+
+}
