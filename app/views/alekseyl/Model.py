3466b00d5f17bb0bb684c70d812414a806afae46
alekseyl1992 <alekseyl@list.ru>
2016-04-25 21:08:11 +0300

    Working on #12 and #15 (using proxy/adapter for now)

---
+++ app/views/alekseyl/Model.py
@@ -0,0 +1,18 @@
+
+class Model:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def create():
+        pass
+
+    def update(self):
+        pass
+
+    @staticmethod
+    def get(pk):
+        pass
+
+    def delete(self):
+        pass
